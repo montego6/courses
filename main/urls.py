f400a45a55e26c0cd0740b0bafdf14e1ef3af89b
montego <montego@Ubuntu22.10.myguest.virtualbox.org>
2023-06-17 00:04:17 +0300

    Working on a header css and html

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from main import views
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('', views.index)
+    path('', TemplateView.as_view(template_name='index.html'))
 ]
