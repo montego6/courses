2cfde96ee0f35e28efde5888f37f85fdf782b95b
Kirill Kirsanov <montego@yandex.ru>
2023-06-17 22:40:19 +0300

    Added categories models and serializers

--- categories/models.py
+++ categories/models.py
@@ -1,6 +1,19 @@
 from django.db import models
+from django.core.validators import MinLengthValidator,RegexValidator
 
 # Create your models here.
 
 class Category(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True, validators=[MinLengthValidator(5, message='Количество символов должно быть больше 4'), 
+                                                                    RegexValidator(r'[А-Яа-я]+')])
+
+
+class SubCategory(models.Model):
+    name = models.CharField(max_length=50, unique=True, validators=[MinLengthValidator(5, message='Количество символов должно быть больше 4'), 
+                                                                    RegexValidator(r'[А-Яа-я]+')])
+    parent_category = models.ForeignKey(Category, related_name='subcategories', on_delete=models.CASCADE)
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    parent_subcategory = models.ForeignKey(SubCategory, related_name='subjects', on_delete=models.CASCADE)
---
+++ categories/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import Category, SubCategory, Subject
+
+
+class SubjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subject
+        fields = ['id', 'name']
+
+
+class SubCategorySerializer(serializers.ModelSerializer):
+    subjects = SubjectSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = SubCategory
+        fields = '__all__'
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    subcategories = SubCategorySerializer(many=True, read_only=True)
+
+    class Meta:
+        model = SubCategory
+        fields = '__all__'
