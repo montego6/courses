a0e0301b8bf53b6146c761a6e2f0629c8dbe1deb
Kirill Kirsanov <montego@yandex.ru>
2023-07-04 22:15:57 +0300

    Frontend course single: adding video player and playlist functionality

--- main/static/course-single.js
+++ main/static/course-single.js
@@ -2,6 +2,37 @@ const courseId = document.getElementById('course-id').textContent
 
 getCourseData()
 
+
+let player = videojs(document.querySelector('.video-js'))
+
+player.playlist([
+    { 
+    name: 'Video1',
+    sources: [{
+        src: '/files/media/courses/lessons/0.0_%D0%92%D0%B2%D0%B5%D0%B4%D0%B5%D0%BD%D0%B8%D0%B5.mp4',
+        type: 'video/mp4'
+    }]
+}, 
+{
+    name: 'video2',
+    sources: [{
+        src: '/files/media/courses/lessons/1.1_%D0%94%D0%BB%D1%8F_%D1%87%D0%B5%D0%B3%D0%BE_%D0%BD%D1%83%D0%B6%D0%BD%D0%B0_%D0%9E%D0%A1.mp4',
+        type: 'video/mp4'
+    }]
+}, {
+    name: 'video3',
+    sources: [{
+        src: '/files/media/courses/lessons/1.2_%D0%9F%D0%B5%D1%80%D0%B2%D0%B0%D1%8F_%D0%9E%D0%A1_._%D0%98%D1%81%D1%82%D0%BE%D1%80%D0%B8%D1%8F_Multics.mp4',
+        type: 'video/mp4'
+    }]
+}  
+])
+player.playlist.autoadvance(0)
+player.on('playlistitem', () => {})
+document.getElementById('ui-next-video').addEventListener('click', event => player.playlist.currentItem(2))
+
+// player.playlistUi()
+
 function getCourseData() {
     fetch(`http://127.0.0.1:8000/api/courses/${courseId}/`).then(response => response.json()).then(data => initializePage(data))
 }
@@ -25,6 +56,8 @@ function initializePage(data) {
         lessonCounter += lessonArr.length
     })
     document.getElementById('course-content-info').textContent = `${data.sections.length} секций - ${lessonCounter} видеоуроков`
+    
+    data.sections.forEach(section => createSection(section))
 
     data.requirements.forEach(requierement => {
         const clone = document.getElementById('template-requirements').content.cloneNode(true)
@@ -34,7 +67,6 @@ function initializePage(data) {
 
     document.getElementById('course-full_description').innerHTML = data.full_description
 
-    data.sections.forEach(section => createSection(section))
 }
 
 function expandSection(event) {
