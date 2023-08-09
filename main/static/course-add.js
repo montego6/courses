e895d4ee65dbe0b58b9472ad75bf55cf1631b23a
Kirill Kirsanov <montego@yandex.ru>
2023-08-10 01:29:09 +0300

    Course add frontend refining finished

--- main/static/course-add.js
+++ main/static/course-add.js
@@ -9,10 +9,46 @@ const confirmBtn = document.getElementById('course-confirm-btn')
 const backwardsBtn = document.getElementById('course-backwards-btn')
 
 const optionDependencies = {
-    'basic': ['basic'],
+    'basic': [],
     'extra': ['basic'],
     'premium': ['basic', 'extra']
 }
+
+const optionRreverseDependencies = {
+    'basic': ['extra', 'premium'],
+    'extra': ['premium'],
+    'premium': []
+}
+
+
+document.querySelectorAll('#options-checkboxes input[type=checkbox]').forEach(checkBox => checkBox.addEventListener('change', event => {
+    const option = event.target.getAttribute('name')
+    if (event.target.checked) {
+        if (optionDependencies[option] != [] && !optionDependencies[option].every(dependency => document.querySelector(`#checkbox-${dependency}`).checked)) {
+            event.target.closest('div.options-row').querySelector('.options-error').classList.remove('hidden')
+            event.target.checked = false
+        } else {
+            document.getElementById(`price-${option}`).classList.remove('hidden')
+            document.querySelectorAll('.options-error').forEach(errorElement => errorElement.classList.add('hidden'))
+        }
+    } else {
+        document.getElementById(`price-${option}`).classList.add('hidden')
+        optionRreverseDependencies[option].forEach(dependant => {
+            if (document.querySelector(`#checkbox-${dependant}`).checked) {
+                document.getElementById(`price-${dependant}`).classList.add('hidden')
+                document.querySelector(`#options-${dependant} .options-error`).classList.remove('hidden')
+                document.querySelector(`#checkbox-${dependant}`).checked = false
+            }
+        })
+
+    }
+}))
+
+document.querySelector('#course-back-to-2step-btn').addEventListener('click', event => {
+    courseAddSecondStep.classList.remove('invisible')
+    courseAddThirdStep.classList.add('invisible')
+})
+
 
 whatWillLearnBtn.addEventListener('click', (event) => {
     const clone = document.getElementById('template-what-will-learn').content.cloneNode(true)
@@ -37,11 +73,24 @@ confirmBtn.addEventListener('click', (event) => {
     const options = {option: "value"}
     learnList.forEach(learn => formData.append('what_will_learn', learn))
     requirementsList.forEach(requirement => formData.append('requirements', requirement))
-    formData.append('options', JSON.stringify(options))
-    fetch('http://127.0.0.1:8000/api/courses/', {
-        method: 'post',
-        body: formData
-    }).then(response => response.json()).then(data => console.log(data))
+    const level = document.getElementById('select-level').value
+    formData.append('level', level)
+    let optionsArr = []
+    document.querySelectorAll('.options-row input:checked').forEach(optionCheckbox => {
+        const option = optionCheckbox.getAttribute('name')
+        const price = document.querySelector(`input[name=${option}-price]`).value
+        const optionData = {
+            option: option,
+            price: price
+        }
+        optionsArr.push(optionData)
+    })
+    formData.append('options', JSON.stringify(optionsArr))
+    console.log(formData)
+    // fetch('http://127.0.0.1:8000/api/courses/', {
+    //     method: 'post',
+    //     body: formData
+    // }).then(response => response.json()).then(data => console.log(data))
 
 })
 
@@ -50,21 +99,8 @@ document.querySelector('#course-third-step-btn').addEventListener('click', event
     courseAddThirdStep.classList.remove('invisible')
 })
 
-document.querySelectorAll('#options-checkboxes input[type=checkbox]').forEach(checkBox => checkBox.addEventListener('change', event => {
-    const option = event.target.getAttribute('name')
-    console.log(optionDependencies[option])
-    if (event.target.checked) {
-        // (!document.querySelector(`#checkbox-${optionDependencies[option]}`).checked)
-        if (!optionDependencies[option].every(dependency => document.querySelector(`#checkbox-${dependency}`).checked)) {
-            console.log('Wrong')
-        } else {
-            document.getElementById(`price-${option}`).classList.remove('hidden')
-        }
-    } else {
-        document.getElementById(`price-${option}`).classList.add('hidden')
-    }
-}))
-    
+
+   
 form.addEventListener('submit', (event) => 
 {
     formData = new FormData(form)
