9ffc43f43afc2222ae89fba1d82460f8e94eab5e
Kirill Kirsanov <montego@yandex.ru>
2023-12-01 23:08:06 +0300

    Testing: tests for signals completed

--- courses/signals/handlers.py
+++ courses/signals/handlers.py
@@ -12,15 +12,16 @@ stripe.api_key = config('STRIPE_KEY')
 @receiver(post_save, sender=AdditionalFile)
 @receiver(post_save, sender=Test)
 @receiver(post_save, sender=Homework)
-def create_section_item(sender, instance, created, **kwargs):
+def create_section_item_handler(sender, instance, created, **kwargs):
     if created:
-        SectionItem.objects.create(content_object=instance, section=instance.section)
+        create_section_item(instance)
+        # SectionItem.objects.create(content_object=instance, section=instance.section)
+
+def create_section_item(instance):
+    SectionItem.objects.create(content_object=instance, section=instance.section)
 
 
-@receiver(post_save, sender=Course)
-def create_stripe_course_item(sender, instance, created, **kwargs):
-    if not created:
-        StripeCourse.objects.filter(course=instance).delete()
+def create_stripe_course_item(instance):
     product = stripe.Product.create(name=instance.name)
     price = stripe.Price.create(
         unit_amount=int(instance.price * 100),
@@ -49,9 +50,49 @@ def create_stripe_course_item(sender, instance, created, **kwargs):
         option_prices[option['option']] = option_dict
     StripeCourse.objects.create(course=instance, product=product['id'], option_prices=option_prices)
 
+def delete_stripe_course_item(instance):
+    StripeCourse.objects.filter(course=instance).delete()
+
+@receiver(post_save, sender=Course)
+def create_stripe_course_item_handler(sender, instance, created, **kwargs):
+    if not created:
+        # StripeCourse.objects.filter(course=instance).delete()
+        delete_stripe_course_item(instance)
+    create_stripe_course_item(instance)
+    # product = stripe.Product.create(name=instance.name)
+    # price = stripe.Price.create(
+    #     unit_amount=int(instance.price * 100),
+    #     currency='rub',
+    #     product=product['id']
+    # )
+    # option_prices = {}
+    # for idx, option in enumerate(instance.options):
+    #     option_dict = {}
+    #     option_dict['price'] = stripe.Price.create(
+    #     unit_amount=int(option['price'] * 100),
+    #     currency='rub',
+    #     product=product['id']
+    #     )['id']
+    #     if idx > 0:
+    #         upgrade = stripe.Product.create(name=instance.name + ' upgrade to option ' + option['option'])
+    #         option_dict['upgrade'] = {}
+    #         for upgradable_option in instance.options[:idx]:
+    #             upgrade_price = option['price'] - upgradable_option['price']
+    #             price = stripe.Price.create(
+    #                 unit_amount=int(upgrade_price * 100),
+    #                 currency='rub',
+    #                 product=upgrade['id']
+    #             )
+    #             option_dict['upgrade'][upgradable_option['option']] = price['id']
+    #     option_prices[option['option']] = option_dict
+    # StripeCourse.objects.create(course=instance, product=product['id'], option_prices=option_prices)
+
 
 @receiver(post_save, sender=Lesson)
-def calculate_video_length(sender, instance, *args, **kwargs):
+def calculate_video_length_handler(sender, instance, *args, **kwargs):
+    calculate_video_length(instance)
+
+
+def calculate_video_length(instance):
     video = VideoFileClip(instance.file.path)
     Lesson.objects.filter(id=instance.id).update(duration=video.duration)
-
---
+++ courses/tests/test_signals.py
@@ -0,0 +1,70 @@
+from unittest.mock import patch
+import pytest
+import factories as ft
+
+
+@pytest.mark.parametrize('factory', [
+    ft.AdditinalFileFactory,
+    ft.TestFactory,
+    ft.HomeworkFactory, 
+    ft.LessonFactory
+])
+@pytest.mark.django_db
+@patch('courses.signals.handlers.create_stripe_course_item')
+def test_signal_create_section_item(mock_class, factory):
+    with patch('courses.signals.handlers.create_section_item') as mock:
+        model = factory()
+        assert mock.called
+        assert mock.call_count == 1
+
+
+@pytest.mark.parametrize('factory', [
+    ft.CourseFactory,
+    ft.CategoryFactory,
+    ft.SubCategoryFactory, 
+    ft.UserFactory,
+])
+@pytest.mark.django_db
+@patch('courses.signals.handlers.create_stripe_course_item')
+def test_signal_create_section_item_fails(mock_class, factory):
+    with patch('courses.signals.handlers.create_section_item') as mock:
+        model = factory()
+        assert not mock.called
+
+
+@pytest.mark.django_db
+def test_signal_create_stripe_course_item():
+    with patch('courses.signals.handlers.create_stripe_course_item') as mock:
+        model = ft.CourseFactory()
+        assert mock.called
+        assert mock.call_count == 1
+
+
+@pytest.mark.django_db
+def test_signal_create_stripe_course_item_fails():
+    with patch('courses.signals.handlers.create_stripe_course_item') as mock:
+        model = ft.CategoryFactory()
+        assert not mock.called
+
+
+@pytest.mark.django_db
+@patch('courses.signals.handlers.create_stripe_course_item')
+def test_signal_calculate_video_length(mock_class):
+    with patch('courses.signals.handlers.calculate_video_length') as mock:
+        model = ft.LessonFactory()
+        assert mock.called
+        assert mock.call_count == 1
+
+
+@pytest.mark.parametrize('factory', [
+    ft.CourseFactory,
+    ft.TestFactory,
+    ft.AdditinalFileFactory, 
+    ft.HomeworkFactory,
+])
+@pytest.mark.django_db
+@patch('courses.signals.handlers.create_stripe_course_item')
+def test_signal_create_section_item_fails(mock_class, factory):
+    with patch('courses.signals.handlers.calculate_video_length') as mock:
+        model = factory()
+        assert not mock.called
