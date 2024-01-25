b975e0b0bf532735237774831ca98b4b9bbfc5c7
Kirill Kirsanov <montego@yandex.ru>
2024-01-25 23:12:59 +0300

    Refactoring: mailing letter templates moved to consts

---
+++ mailings/consts.py
@@ -0,0 +1,5 @@
+REGISTRATION_LETTER_BODY = "Welcome to courses, dear {}"
+REGISTRATION_LETTER_HEADER = "Welcome to courses"
+
+COURSE_BUY_LETTER_HEADER = "You have bought course {}"
+COURSE_BUY_LETTER_BODY =  "Congratulations, dear {}! You have bought course {}. Your option is {}. If you want to upgrade you can do it on a course page http://127.0.0.1:8000/course/{}/"
--- mailings/tasks.py
+++ mailings/tasks.py
@@ -1,12 +1,13 @@
 from celery import shared_task
 from django.core.mail import send_mail
+import consts
 
 
 @shared_task
 def send_registration_letter(email, username):
     send_mail(
-        "Welcome to courses",
-        f"Welcome to courses, dear {username}",
+        consts.REGISTRATION_LETTER_HEADER,
+        consts.REGISTRATION_LETTER_BODY.format(username),
         None,
         [email],
         fail_silently=False,
@@ -16,11 +17,8 @@ def send_registration_letter(email, username):
 @shared_task
 def send_course_buy_letter(email, username, course_id, course_name, option):
     send_mail(
-        f"You have bought course {course_name}",
-        f"""Congratulations, dear {username}! You have bought course {course_name}.
-        Your option is {option}. If you want to upgrade 
-        you can do it on a course page http://127.0.0.1:8000/course/{course_id}/
-         """,
+        consts.COURSE_BUY_LETTER_HEADER.format(course_name),
+        consts.COURSE_BUY_LETTER_BODY.format(username, course_name, option, course_id),
          None,
          [email],
          fail_silently=False
