ff3a037f5c496b9b9fdf07fcd3ad8876c650b707
Kirill Kirsanov <montego@yandex.ru>
2024-01-26 23:00:06 +0300

    Admin page: course admin model finished

--- courses/admin.py
+++ courses/admin.py
@@ -2,15 +2,28 @@ from django.contrib import admin
 from django.utils.http import urlencode
 from django.urls import reverse
 from django.utils.html import format_html
+
+from users.helpers import get_user_full_name
 from .models import Course, Section, StripeCourse, CoursePayment
 
+@admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    list_display = ['id', 'bname', 'author', 'subject', 'total_sales', 'show_students_link', 'is_published']
+    list_display = ['id', 'bname', 'author_fullname_link', 'author_id', 'subject', 'total_sales', 'show_students_link', 'is_published']
     list_display_links = ['id', 'bname']
+    list_filter = ['subject__parent_subcategory__parent_category', 'is_published', 'is_free']
+    search_fields = ['name', 'author__id', 'author__first_name', 'author__last_name']
+
+    def author_id(self, obj):
+        return obj.author.id
 
     def bname(self, obj):
         return format_html('<b>{}</b>', obj.name)
-    
+
+    def author_fullname_link(self, obj):
+        full_name = get_user_full_name(obj.author)
+        url = reverse('admin:courses_course_changelist') + '?' + urlencode({'author__id': obj.author.id})
+        return format_html('<a href="{}">{}</a>', url, full_name)
+
     def show_students_link(self, obj):
         count = obj.num_students()
         url = reverse('admin:auth_user_changelist') + '?' + urlencode({'student_courses__id': obj.id})
@@ -18,11 +31,12 @@ class CourseAdmin(admin.ModelAdmin):
 
     bname.short_description = 'Name'
     show_students_link.short_description = 'Number of students'
+    author_fullname_link.short_description = 'Author'
 
 
-admin.site.register(Course, CourseAdmin)
+
+
 admin.site.register(Section)
 
 admin.site.register(StripeCourse)
 admin.site.register(CoursePayment)
-# Register your models here.
